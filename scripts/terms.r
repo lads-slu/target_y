872063f45f05bfd2cca1da5502b09a70c403cc64
Kristin Persson <kristin.persson@slu.se>
2024-02-06 13:11:51 +0100

    workdlow stramlined

--- scripts/terms.r
+++
@@ -1,8 +0,0 @@
-####ymaps####
-#version: SEE version.r
-#authors: Mats Söderström (SLU), Kristin Persson (SLU), Henrik Stadig (Entorp lantbruk)
-#Before use: read terms.r
-#########################
-
-'Terms of use; For licence, disclaimer and acknowledgement on how to cite see: 
-  terms\\terms.doc'
